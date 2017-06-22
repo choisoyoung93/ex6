706ac431dd4bf3e11004beef6fc192aa764406fe
user2-PC\user2 <csoyoung61@gmail.com>
2017-06-22 20:15:21 +0900

    Basic Setting

---
+++ src/test/java/com/choa/ex6/ConnectionTest.java
@@ -0,0 +1,17 @@
+package com.choa.ex6;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ConnectionTest extends MyAbstractTest{
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Test
+	public void test(){
+		assertNotNull(sqlSession);
+	}
+}
